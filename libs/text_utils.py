2330eba809e829ea345516b2400e97bbeae55310
Vicente Garofalo <vicente.garofalo@avangenio.com>
2023-06-04 15:34:31 -0400

    feat: docx file support and function to support all text files via textract (not implemented)

--- libs/text_utils.py
+++ libs/text_utils.py
@@ -1,5 +1,26 @@
+import io, textract, tempfile, os
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from docx import Document
+
+
+def get_text(doc):
+    # Create a temporary file
+    with tempfile.NamedTemporaryFile(delete=False) as temp:
+        # Write the uploaded file data to the temporary file
+        temp.write(doc.read())
+        # Get the path of the temporary file
+        temp_path = temp.name
+
+    # Determine the file extension
+    extension = doc.name.split('.')[-1]
+    # Extract the text from the temporary file using textract
+    text = textract.process(temp_path, extension=extension).decode('utf-8')
+
+    # Delete the temporary file
+    os.remove(temp_path)
+
+    return text
 
 
 def get_pdf_text(pdf_docs):
@@ -8,6 +29,16 @@ def get_pdf_text(pdf_docs):
         pdf_reader = PdfReader(pdf)
         for page in pdf_reader.pages:
             text += page.extract_text()
+    return text
+
+
+def get_docx_text(doc):
+    # Load the docx file data into a stream
+    doc_stream = io.BytesIO(doc.read())
+    # Create a Document object from the stream
+    document = Document(doc_stream)
+    # Extract the text from the document
+    text = '\n'.join([para.text for para in document.paragraphs])
     return text
 
 
