7f100834a60c2c81846cf674e857f04a2126907e
lauraglza63 <lauraglza63@gmail.com>
2023-06-23 17:36:43 -0400

    feat: Tool for QA over csv: input is a binary file

--- libs/tools.py
+++ libs/tools.py
@@ -5,6 +5,7 @@ from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.agents.agent import AgentExecutor
 from langchain.agents import create_csv_agent
+from libs.table_formats_agent import create_table_format_agent
 import tempfile, os
 
 
@@ -30,7 +31,25 @@ def get_csv_tool_by_route(route_user_csv: str) -> Tool:
     return Tool(
         name='get_csv_information',
         func=agent.run,
-        description="A search tool. Mandatory to find information that is relevant to the user's question inside a csv, xlsx or ods document. The entry must be a search query."
+        description="A search tool. Mandatory to find information that is relevant to the user's question inside a csv document. The entry must be a search query."
     )
 
 
+def get_csv_tool_by_file(user_csv) -> Tool:
+
+    with tempfile.NamedTemporaryFile(delete=False, prefix="temporal_file_of_data",dir='./demo') as temp_file:
+        temp_file.write(user_csv.read())
+        temp_file.seek(0)
+        temp_path = temp_file.name
+
+    agent: AgentExecutor = create_csv_agent(OpenAI(temperature=0), temp_path, verbose=False, max_iterations=3)
+
+    os.remove(temp_path)
+
+    return Tool(
+        name='get_csv_information',
+        func=agent.run,
+        description="A search tool. Mandatory to find information that is relevant to the user's question inside a csv document. The entry must be a search query."
+    )
+
+    #A search tool. Mandatory to find information that is relevant to the user's question inside a csv, xlsx or ods document. The entry must be a search query.
